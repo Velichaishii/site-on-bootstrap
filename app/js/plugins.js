5d69d536fb38b8b4d787e263c694a7b8a4e5d317
Максим <zhil.byl.max@gmail.com>
2016-10-21 14:26:21 +0600

    Version 0.9.beta.

--- app/js/plugins.js
+++ app/js/plugins.js
@@ -2,21 +2,6 @@ $(document).ready(function() {
 
     var doc = $(document);
 
-
-    doc.click(function (e) {
-        var target = $(e.target),
-            popup = target.closest(".popup");
-        popup_open = target.closest(".popup_open");
-
-        if ((!popup.length > 0 ) && (!popup_open.length > 0 )) {
-            $(".popup").removeClass("visible");
-            $(".site-wrap__overlay").fadeOut(400);
-        }
-
-    });
-
-
-
     doc.on("click", ".slider-b__item", function(e){
         var index_el = $(this).data("slide");
         doc.find(".slider-b__item").removeClass("active");
